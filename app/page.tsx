3467c86d7bda148946f48dd075d109b51a75c8c8
Arman Mia <mdarman365506@gmail.com>
2025-12-26 02:04:03 +0600

    ci: add github action for railway deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen text-5xl bg-white text-black">
-      <h1>Hello Docker!</h1>
+      <h1>Hello Docker with github action!</h1>
    </div>
   );
 }
