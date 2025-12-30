702192e0c6287e9c740c4fc66bbae8077a4f59a6
Arman Mia <mdarman365506@gmail.com>
2025-12-30 18:58:53 +0600

    test: gpg signing check

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen text-5xl bg-white text-black">
    
-      <h1>Hello Docker 🚀 Updated Version!</h1>
+      <h1>Hello Docker 🚀 Updated Version</h1>
     </div>
   );
 }
